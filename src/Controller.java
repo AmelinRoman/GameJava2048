9113c7fb4618d3d389dfe42edbfa4e2b3cf31fb3
AmelinRoman <94740458+AmelinRoman@users.noreply.github.com>
2023-04-23 15:23:53 +0300

    Реализация класса Controller 1. Наследуем класс от KeyAdapter, для работы с клавиатурой 2. Получаем доступ к классу Model и его полям 3. Получаем доступ к классу View(пока заглушка) и к его методам 4. Реализуем метод перезагрузки игры 5. Реализуем метод обработки нажатий и выполнение методов Model, в зависимости от нажатия

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,54 @@
-public class Controller {
+import tile.Tile;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Controller extends KeyAdapter { // Наследуемся от KeyAdapter для работы с клавиатурой
+    private Model model;
+    private View view;
+    private static final int WINNING_TILE = 2048; // Номер победной плитки
+
+    public Controller(Model model) {
+        this.model = model;
+        this.view = new View(this); // Инициализируем View текущим контроллером
+    }
+
+    public Tile[][] getGameTiles() { return  model.getGameTiles(); } // Получаем игровое поле от модели
+    public int getScore() { return model.score; } // Получаем от модели текущий счет
+    public View getView() { return view; } // Получаем представление
+    // Метод для загрузки игры сначала
+    public void resetGame() {
+        this.model.score = 0;
+        // флаги определяющие победил игрок или проиграл
+        view.isLoseGame = false;
+        view.isWinnerGame = false;
+        // обновляем поле игры через модель
+        model.resetGame();
+    }
+
+    // Переопределяем метод KeyAdapter для обработки нажатий  на клавиатуру
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // Если пользователь нажал ESC, перезагружаем игру
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            resetGame();
+        }
+        if (!model.canMove()) { // Если не можем сделать ход
+            view.isLoseGame = true; // Выставляем флаг проиграл
+        }
+        // Проверяем что мы не победили и не проиграли и обрабатываем нажатия на клавиатуру определенными методами
+        if (!view.isLoseGame && !view.isWinnerGame) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_RIGHT -> model.right();
+                case KeyEvent.VK_LEFT -> model.left();
+                case KeyEvent.VK_UP -> model.up();
+                case KeyEvent.VK_DOWN -> model.down();
+            }
+        }
+        // Проверяем если максимальная плитка равна 2048, если равна выставляем флаг победителя
+        if (model.maxTile == WINNING_TILE) {
+            view.isWinnerGame = true;
+        }
+        view.repaint(); // Обновляем
+    }
 }
